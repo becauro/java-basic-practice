f7e0ea0c5db4849716ca666dbac811539a531699
Michel Pereira <michelbecauro@gmail.com>
2022-04-21 04:09:38 -0300

    Practices Codes from Java web Alex's course

---
+++ src/cursojava/executavel/UmaClassJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class UmaClassJava {
+	
+	/* main é um metodo autoexecutável em Java*/
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Testando saída em Java");
+		System.out.println("Testando é OO");
+	}
+
+}
