fb4c34d882c8c5b4551e8e7d77545b1308c5aa50
Michel Pereira <michelbecauro@gmail.com>
2022-04-24 06:57:08 -0300

    feat: Add visibility practicec

--- src/cursojava/classes/Aluno.java
+++ src/cursojava/classes/Aluno.java
@@ -2,9 +2,25 @@ package cursojava.classes;
 
 public class Aluno {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+	public Aluno() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Aluno(String nameAluno, int idadeAluno) {
+		nome = nameAluno;
+		idade = idadeAluno;
+		
 	}
 
 }
--- src/cursojava/executavel/UmaClassJava.java
+++ src/cursojava/executavel/UmaClassJava.java
@@ -1,19 +1,21 @@
 package cursojava.executavel;
 
+import cursojava.classes.Aluno;
+
 public class UmaClassJava {
 	
 	/* main é um metodo autoexecutável em Java*/
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Testando saída em Java");
-		System.out.println("Testando é OO");
-		System.out.println("Testando Eclipse com Github");
-		System.out.println("Segundo teste com Eclipse e Github");
-		System.out.println("Eclipse já com token nas configurações");
-		System.out.println("Eclipse e github. Outro teste");
-		System.out.println("Eclipse e github. Outro teste2");
-		System.out.println("Eclipse e github. token in store");
-	}
 
+		Aluno aluno1 = new Aluno();
+		
+		Aluno aluno2 = new Aluno("Torquato", 64); 
+		
+		
+		System.out.println("Nome do aluno: " + aluno2.nome);
+	}
+	
+		
 }
