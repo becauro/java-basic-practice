e6bd9dcf72f4a308ab6a10b5d360be17488cfbfa
Michel Pereira <michelbecauro@gmail.com>
2022-04-27 10:18:41 -0300

    feat: Create 'Disciplina' class

--- src/cursojava/classes/Aluno.java
+++ src/cursojava/classes/Aluno.java
@@ -14,17 +14,14 @@ public class Aluno {
 	String dataMatricula;
 	String nomeEscola;
 	String serieMatriculado;
-	double nota1;
-	double nota2;
-	double nota3;
-	double nota4;
-
-	String disciplina1;
-	String disciplina2;
-	String disciplina3;
-	String disciplina4;
-
-
+	
+	Disciplina disciplina = new Disciplina();
+	
+	
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+	
 	public Aluno(String nameAluno, int idadeAluno) {
 		nome = nameAluno;
 		idade = idadeAluno;
@@ -110,75 +107,11 @@ public class Aluno {
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
 	}
-
-	public double getNota1() {
-		return nota1;
-	}
-
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-
-	public double getNota2() {
-		return nota2;
-	}
-
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-
-	public double getNota3() {
-		return nota3;
-	}
-
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-
-	public double getNota4() {
-		return nota4;
-	}
-
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
-	}
 	
-
-	public String getDisciplina1() {
-		return disciplina1;
-	}
-
-	public void setDisciplina1(String disciplina1) {
-		this.disciplina1 = disciplina1;
-	}
-
-	public String getDisciplina2() {
-		return disciplina2;
-	}
-
-	public void setDisciplina2(String disciplina2) {
-		this.disciplina2 = disciplina2;
-	}
-
-	public String getDisciplina3() {
-		return disciplina3;
-	}
-
-	public void setDisciplina3(String disciplina3) {
-		this.disciplina3 = disciplina3;
-	}
-
-	public String getDisciplina4() {
-		return disciplina4;
-	}
-
-	public void setDisciplina4(String disciplina4) {
-		this.disciplina4 = disciplina4;
-	}
-
 	
 	public double getMedia() {
-		return (nota1 + nota2 + nota3 + nota4) / 4;
+		return (disciplina.getNota1() + disciplina.getNota2() + 
+				disciplina.getNota3() + disciplina.getNota4()) / 4;
 	}
 	
 	public boolean getAlunoAprovado() {
@@ -199,9 +132,7 @@ public class Aluno {
 		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
 				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
 				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
-				+ serieMatriculado + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", nota4=" + nota4
-				+ ", disciplina1=" + disciplina1 + ", disciplina2=" + disciplina2 + ", disciplina3=" + disciplina3
-				+ ", disciplina4=" + disciplina4 + "]";
+				+ serieMatriculado + ", disciplina=" + disciplina + "]";
 	}
 
 	@Override
