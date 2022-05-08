402293b0219563215fa98d3afb23128e122ce1a3
Michel Pereira <michelbecauro@gmail.com>
2022-05-08 16:17:02 -0300

    feat: Create 'Pessoa' superclass

--- src/cursojava/classes/Aluno.java
+++ src/cursojava/classes/Aluno.java
@@ -2,115 +2,78 @@ package cursojava.classes;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import cursojava.constantes.StatusAluno;
 
-public class Aluno {
+public class Aluno extends Pessoa {
 
-	String nome;
-	int idade;
-	String dataNascimento;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
+
 	String dataMatricula;
 	String nomeEscola;
 	String country;
 	String serieMatriculado;
-	
+
 	List<Disciplina> disciplinas = new ArrayList<Disciplina>();
-	
-	
-	public List<Disciplina> getDisciplinas() {
-		return disciplinas;
-	}
 	
 	public Aluno(String nomeEscola, String country) {
 		this.nomeEscola = nomeEscola;
 		this.country = country;
 	}
+	
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public int getIdade() {
-		return idade;
-	}
-
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public String getRegistroGeral() {
-		return registroGeral;
-	}
-
-	public void setRegistroGeral(String registroGeral) {
-		this.registroGeral = registroGeral;
-	}
-
-	public String getNumeroCpf() {
-		return numeroCpf;
-	}
-
-	public void setNumeroCpf(String numeroCpf) {
-		this.numeroCpf = numeroCpf;
-	}
-
-	public String getNomeMae() {
-		return nomeMae;
-	}
-
-	public void setNomeMae(String nomeMae) {
-		this.nomeMae = nomeMae;
-	}
-
-	public String getNomePai() {
-		return nomePai;
-	}
-
-	public void setNomePai(String nomePai) {
-		this.nomePai = nomePai;
-	}
 
 	public String getDataMatricula() {
 		return dataMatricula;
 	}
 
+
+
 	public void setDataMatricula(String dataMatricula) {
 		this.dataMatricula = dataMatricula;
 	}
+
+
 
 	public String getNomeEscola() {
 		return nomeEscola;
 	}
 
+
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+
 	public String getSerieMatriculado() {
 		return serieMatriculado;
 	}
 
+
+
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
 	}
-		
-	public String getCountry() {
-		return country;
+
+
+
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
 	}
+
 
 	public double getMedia() {
 		
@@ -139,34 +102,19 @@ public class Aluno {
 			
 			return StatusAluno.REPROVADO;
 		}
-	} 
-
+	}
 
 
 
 	@Override
 	public String toString() {
-		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
-				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
-				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", country=" + country
-				+ ", serieMatriculado=" + serieMatriculado + ", disciplinas=" + disciplinas + "]";
+		return "Aluno [dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", country=" + country
+				+ ", serieMatriculado=" + serieMatriculado + ", disciplinas=" + disciplinas + ", nome=" + nome
+				+ ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral=" + registroGeral
+				+ ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai + "]";
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(nome);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Aluno other = (Aluno) obj;
-		return Objects.equals(nome, other.nome);
-	}
+	
+	
+	
 	
 }
